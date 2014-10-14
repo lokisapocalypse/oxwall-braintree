a04f9d147a3ec651de45639357eee5627c3f9b74
Brian Lamb <fusani.applications@gmail.com>
2014-10-14 02:32:37 +0000

    Admin settings form renders

--- classes/braintree_adapter.php
+++ classes/braintree_adapter.php
@@ -10,9 +10,29 @@ class LATTERDATEBRAINTREEBILLING_CLASS_BraintreeAdapter implements OW_BillingAda
         \Braintree_Configuration::publicKey('jqk3mmxn376st37c');
         \Braintree_Configuration::privateKey('0bc6bbfa014b8371da7e873189a3b43a');
 */
+    public function getFields($params = NULL)
+    {
+        throw new Exception('Not yet implemented');
+    }
+
     public function getLogoUrl()
     {
         $plugin = OW::getPluginManager()->getPlugin('latterdatebraintreebilling');
         return $plugin->getStaticUrl() . 'img/braintree.png';
+    }
+
+    public function getOrderFormUrl()
+    {
+        throw new Exception('Not yet implemented');
+    }
+
+    public function prepareSale(BOL_BillingSale $sale)
+    {
+        throw new Exception('Not yet implemented');
+    }
+
+    public function verifySale(BOL_BillingSale $sale)
+    {
+        throw new Exception('Not yet implemented');
     }
 }
