3755078e13c7c43ef0b043608d7b543be4746654
Brian Lamb <fusani.applications@gmail.com>
2014-11-02 21:36:25 +0000

    Fixed bug where it would not show up as a payment gateway

--- install.php
+++ install.php
@@ -1,24 +1,29 @@
 <?php
 
+// load the billing service
 $billingService = BOL_BillingService::getInstance();
 
+// build a new gateway
 $gateway = new BOL_BillingGateway();
 $gateway->gatewayKey = 'billingbraintree';
 $gateway->adapterClassName = 'BILLINGBRAINTREE_CLASS_BraintreeAdapter';
 $gateway->active = 0;
 $gateway->mobile = 0;
 $gateway->recurring = 1;
-$gateway->currencies = 'AUD,BRL,CAD,CZK,DKK,EUR,HKD,HUF,ILS,JPY,MYR,MXN,NOK,NZD,PHP,PLN,GBP,SGD,SEK,CHF,TWD,THB,USD';
+$gateway->currencies = 'USD';
 
+// add the gateway to the billing service
 $billingService->addGateway($gateway);
+
+// set some defaults for this gateway
 $billingService->addConfig('billingbraintree', 'business', '');
 $billingService->addConfig('billingbraintree', 'publicKey', '');
 $billingService->addConfig('billingbraintree', 'privateKey', '');
 $billingService->addConfig('billingbraintree', 'sandboxMode', '0');
 
+// define the admin settings page
 OW::getPluginManager()->addPluginSettingsRouteName('billingbraintree', 'braintree_billing_admin');
 
+// load the language keys
 $path = OW::getPluginManager()->getPlugin('billingbraintree')->getRootDir() . 'langs.zip';
 OW::getLanguage()->importPluginLangs($path, 'billingbraintree');
-
-BOL_LanguageService::getInstance()->addPrefix('billingbraintree', 'Braintree Billing');
